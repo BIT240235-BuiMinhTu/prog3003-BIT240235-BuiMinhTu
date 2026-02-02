12965eeaf232a24af784cf2e355aa280f4e68224
MinhTuisme <nguyenthuhuong270680@gmail.com>
2026-02-02 07:32:24 +0700

    Add files via upload

---
+++ Chapter01/Bai6.java
@@ -0,0 +1,16 @@
+package Baitap;
+
+import java.util.HashSet;
+
+public class Bai6 {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 2, 5, 8, 1};
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int x : arr) {
+            set.add(x);
+        }
+
+        System.out.println(set);
+    }
+}
