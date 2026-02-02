0bf6241d37e935bac0cb2b9baee5387629dbe041
MinhTuisme <nguyenthuhuong270680@gmail.com>
2026-02-02 09:06:54 +0700

    Add files via upload

---
+++ Chapter3/Bai1.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Baitap;
+
+/**
+ *
+ * @author laptop dell
+ */
+class WorkerThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Thread đang chạy...");
+    }
+}
+
+class WorkerRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Runnable đang chạy...");
+    }
+}
+
+public class Bai1 {
+    public static void main(String[] args) {
+        WorkerThread t1 = new WorkerThread();
+        Thread t2 = new Thread(new WorkerRunnable());
+
+        t1.start();
+        t2.start();
+    }
+}
+
