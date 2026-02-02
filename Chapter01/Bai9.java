f0d078a55d4282ae0dd249299b528d843ea854bf
MinhTuisme <nguyenthuhuong270680@gmail.com>
2026-02-02 07:34:14 +0700

    Add files via upload

---
+++ Chapter01/Bai9.java
@@ -0,0 +1,21 @@
+package Baitap;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Bai9 {
+    public static void main(String[] args) {
+        TreeMap<String, Double> products = new TreeMap<>();
+
+        products.put("Laptop", 1500.0);
+        products.put("Mouse", 25.0);
+        products.put("Keyboard", 50.0);
+
+        System.out.println(products);
+
+        Map<String, Double> sub =
+                products.subMap("K", true, "M", true);
+
+        System.out.println(sub);
+    }
+}
